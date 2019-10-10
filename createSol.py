880c3d29d2924ea2b30baa62dc1fe720646bb374
Claire Soyez-Martin <csoyezmarti@emi.u-bordeaux1.fr>
2019-10-10 12:12:27 +0200

    intercept

--- createSol.py
+++ createSol.py
@@ -1,8 +1,28 @@
 from graph import *
+from problem import *
+from math import sqrt, floor
 
 class createSol :
     def __init__(self, problem):
         self.problem = problem
         self.graph = graph()
+        self.step = floor(problem.robot_radius / problem.pos_step)
 
-    def
+    def intercept(start_shot, end_shot): #hyp : start_shot and end_shot are integers
+        #TODO : verify if x, y are in the field
+        ret = []
+        x_min = min(start_shot[0], end_shot[0]) - self.step
+        x_max = max(start_shot[0], end_shot[0]) + self.step
+        y_min = min(start_shot[1], end_shot[1]) - self.step
+        y_max = max(start_shot[1], end_shot[1]) + self.step
+        for x in range(x_min, x_max+1):
+            for y in range(y_min, y_max+1):
+                inter = segmentCircleIntersection(start_shot, end_shot, [x,y], self.problem.robot_radius)
+                if inter != None:
+                    add = False
+                    for i in len(self.problem.opponents[0]):
+                        d = sqrt((x-self.problem.opponents[0][i])**2 + (y-self.problem.opponents[1][i])**2)
+                        add = add and (d > self.problem.robot_radius) #not optimal
+                    if add:
+                        ret.append([x,y])
+        return ret
