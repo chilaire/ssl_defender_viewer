04691c9590918f6a04970cba3d5a331ed12c6d18
Claire Soyez-Martin <csoyezmarti@emi.u-bordeaux1.fr>
2019-11-28 09:07:09 +0100

    Re-added exec choice

--- mainBis.py
+++ mainBis.py
@@ -8,7 +8,7 @@ from solution import *
 from board import *
 
 if (len(sys.argv) < 2) :
-    sys.exit("Usage: " + sys.argv[0] + " <problem.json>")
+    sys.exit("Usage: " + sys.argv[0] + " <problem.json>" + " (greedy \ exact)" + " default case:exact")
 
 problem_path = sys.argv[1]
 
@@ -19,8 +19,19 @@ s = createSol(problem)
 s.create_graph()
 for k in range(1,len(s.problem.opponents[0])*2+1):
     print(k)
-    if s.dom_ind_set_glouton(k):
-        break
+    if len(sys.argv) == 3 :
+        if sys.argv[2].lower() == "greedy" :
+            print("Executing greedy method...")
+            if s.dom_ind_set_glouton(k):
+                break
+        else :
+            print("Executing exact method...")
+            if s.dom_ind_set(k):
+                break
+    else :
+        print("Executing exact method...")
+        if s.dom_ind_set(k):
+            break
 
 solutionPb = s.get_solution()
 print("solution :",solutionPb)
